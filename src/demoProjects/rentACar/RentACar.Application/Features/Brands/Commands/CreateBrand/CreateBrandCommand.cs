6a0678a0f8095b20c806a31ff16e4572fb687239
Emre Kisaboyun <emre.kisaboyun@bs.nttdata.com>
2022-09-07 18:41:03 +0300

    CreateBrandCommand and Controller added.

---
+++ src/demoProjects/rentACar/RentACar.Application/Features/Brands/Commands/CreateBrand/CreateBrandCommand.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using MediatR;
+using RentACar.Application.Features.Brands.Dtos;
+using RentACar.Application.Services.Repositories;
+using RentACar.Domain.Entities;
+
+namespace RentACar.Application.Features.Brands.Commands.CreateBrand
+{
+    // MediatR' dan gelen IRequest' e boyle bir command geldiginde, geri dondurmesini istedigimiz Dto yu bildirmeliyiz.
+    public class CreateBrandCommand : IRequest<CreatedBrandDto>
+    {
+        public string Name { get; set; }
+
+        // Bu class' i istersek CreateBrand klasorunun altinda ayriyeten olusturabilirdik. Ama burada olmasininda bir sakincasi yoktur.
+        public class CreateBrandCommandHandler : IRequestHandler<CreateBrandCommand, CreatedBrandDto>
+        {
+            private readonly IBrandRepository _brandRepository;
+            private readonly IMapper _mapper;
+
+            public CreateBrandCommandHandler(IBrandRepository brandRepository, IMapper mapper)
+            {
+                this._brandRepository = brandRepository;
+                this._mapper = mapper;
+            }
+
+            public async Task<CreatedBrandDto> Handle(CreateBrandCommand request, CancellationToken cancellationToken)
+            {
+                Brand mappedBrand = _mapper.Map<Brand>(request);
+                Brand createdBrand = await _brandRepository.AddAsync(mappedBrand);
+                CreatedBrandDto createdBrandDto = _mapper.Map<CreatedBrandDto>(createdBrand);
+                return createdBrandDto;
+            }
+        }
+    }
+}
--- src/demoProjects/rentACar/RentACar.WebAPI/Program.cs
+++ src/demoProjects/rentACar/RentACar.WebAPI/Program.cs
@@ -1,3 +1,4 @@
+using RentACar.Application;
 using RentACar.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,7 +6,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddApplicationServices();
+//builder.Services.AddSecurityServices();
 builder.Services.AddPersistenceServices(builder.Configuration);
+//builder.Services.AddInfrastructureServices();
+//builder.Services.AddHttpContextAccessor();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
